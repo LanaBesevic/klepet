1d6d2a89685e02118e320dd7e4f32285714052a8
Lana Besevic <lana.besevic@gmail.com>
2016-04-09 13:23:49 +0000

    Zaključek dela na I. nalogi

--- public/js/klepet_UI.js
+++ public/js/klepet_UI.js
@@ -99,6 +99,12 @@ $(document).ready(function() {
     for (var i=0; i < uporabniki.length; i++) {
       $('#seznam-uporabnikov').append(divElementEnostavniTekst(uporabniki[i]));
     }
+    
+    $('#seznam-uporabnikov div').click(function() {
+     var sporocilo = $('#poslji-sporocilo');
+     sporocilo.val('/zasebno "' + $(this).text() + '"');
+     sporocilo.focus();
+    });
   });
 
   setInterval(function() {
